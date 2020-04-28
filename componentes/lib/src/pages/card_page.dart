797c7915558387fc8f93127744728e065ee264ca
codemvs <codemvs@gmail.com>
2020-04-27 23:53:42 -0500

    Se agrega seccion cards, agregar imagen con preloader

--- componentes/lib/src/pages/card_page.dart
+++ componentes/lib/src/pages/card_page.dart
@@ -8,14 +8,16 @@ class CardPage extends StatelessWidget{
       ),
       body: ListView(
         children: <Widget>[
-          _cardTipo1()
+          _cardTipo1(),
+          SizedBox(height: 30.0,),
+          _cardTipo2(),
         ],
         padding: EdgeInsets.all(10.0),
       ),
     );
   }
 
-  _cardTipo1() {
+  Widget _cardTipo1() {
     return Card(
       child: Column(
         children: <Widget>[
@@ -28,7 +30,9 @@ class CardPage extends StatelessWidget{
             children: <Widget>[
               FlatButton(
                 child: Text('Cancelar'),
-                onPressed: (){},
+                onPressed: (){
+                  
+                },
               ),
               FlatButton(
                 child: Text('Ok'),
@@ -36,6 +40,33 @@ class CardPage extends StatelessWidget{
               )
             ],
             mainAxisAlignment: MainAxisAlignment.end,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          // Cargar imagen con preloader
+          FadeInImage(
+            image: NetworkImage('https://www.publicdomainpictures.net/pictures/30000/velka/evening-landscape-13530956185Aw.jpg'),
+            placeholder: AssetImage('assets/original.gif'),
+            fadeInDuration: Duration(milliseconds: 200),
+            height: 300.0,
+            fit: BoxFit.cover,
+
+          ),
+          // Imanges carga nomrmal
+          // Image( 
+          //   image: NetworkImage('https://www.publicdomainpictures.net/pictures/30000/velka/evening-landscape-13530956185Aw.jpg')
+          // ),
+          
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Loremo impusm')
           )
         ],
       ),
