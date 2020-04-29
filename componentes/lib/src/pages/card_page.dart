e87ed2643f53a61e9681be9ece3b24047d4b1b24
codemvs <codemvs@gmail.com>
2020-04-28 22:48:59 -0500

    Se agrega card tipo configurado

--- componentes/lib/src/pages/card_page.dart
+++ componentes/lib/src/pages/card_page.dart
@@ -11,6 +11,23 @@ class CardPage extends StatelessWidget{
           _cardTipo1(),
           SizedBox(height: 30.0,),
           _cardTipo2(),
+          SizedBox(height: 30.0,),
+          _cardTipo1(),
+          SizedBox(height: 30.0,),
+          _cardTipo2(),
+          SizedBox(height: 30.0,),
+          _cardTipo1(),
+          SizedBox(height: 30.0,),
+          _cardTipo2(),
+          SizedBox(height: 30.0,),
+          _cardTipo1(),
+          SizedBox(height: 30.0,),
+          _cardTipo2(),
+          SizedBox(height: 30.0,),
+          _cardTipo1(),
+          SizedBox(height: 30.0,),
+          _cardTipo2(),
+          SizedBox(height: 30.0,),
         ],
         padding: EdgeInsets.all(10.0),
       ),
@@ -19,6 +36,8 @@ class CardPage extends StatelessWidget{
 
   Widget _cardTipo1() {
     return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
       child: Column(
         children: <Widget>[
           ListTile(
@@ -47,7 +66,8 @@ class CardPage extends StatelessWidget{
   }
 
   Widget _cardTipo2() {
-    return Card(
+    final card = Container(
+      // clipBehavior: ,
       child: Column(
         children: <Widget>[
           // Cargar imagen con preloader
@@ -69,6 +89,24 @@ class CardPage extends StatelessWidget{
             child: Text('Loremo impusm')
           )
         ],
+      ),
+    );
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0),
+        color: Colors.white,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10.0,
+            spreadRadius: 2.0,
+            offset: Offset(2.0,10.0)
+          )
+        ]
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30.0),
+        child: card,
       ),
     );
   }
